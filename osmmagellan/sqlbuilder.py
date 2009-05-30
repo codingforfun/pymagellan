aa6fbdcb73f31bdf0a8bd4b87990fba3d45bb8ef
lhj <lhj@cffc49bd-a764-474f-9146-68f7a4196ebd>
2009-05-30 07:16:07 +0000

    Added bounding box support to database reader
    git-svn-id: https://pymag.svn.sourceforge.net/svnroot/pymag/pymagellan/trunk@27 cffc49bd-a764-474f-9146-68f7a4196ebd

--- osmmagellan/sqlbuilder.py
+++ osmmagellan/sqlbuilder.py
@@ -5,9 +5,11 @@ import osm
 
 class SQLBuilder(object):
     """Parse a rule file and build OSM PostgreSQL queries"""
-    def __init__(self):
+    def __init__(self, box_way_list=None, box_node_list=None):
         self.rulestack = []
         self.result = []
+        self.box_way_list = box_way_list
+        self.box_node_list = box_node_list        
 
     def start(self, tag, attrib):   # Called for each opening tag.
         if tag == 'rule':
@@ -24,9 +26,11 @@ class SQLBuilder(object):
                     raise ValueError('Nested rules must refer to same element')
                 
             if element == 'way':
-                query = self._make_query('way_tags', 'way_id')
+                query = self._make_query('way_tags', 'way_id',
+                                         self.box_way_list)
             elif element == 'node':
-                query = self._make_query('node_tags', 'node_id')
+                query = self._make_query('node_tags', 'node_id',
+                                         self.box_node_list)
 
             self.result.append((self.statement, element, query))
 
@@ -38,39 +42,69 @@ class SQLBuilder(object):
     def close(self):    # Called when all data has been parsed.
         return self.maxDepth
 
-    def _make_query(self, table, id):
+    def _make_query(self, table, id, box_list):
         query = 'SELECT %s ' \
-            'FROM %s ' \
-            'WHERE '%(id, table)
+            'FROM %s '%(id, table)
 
-        conditions = []
+        if box_list != None:
+            query += 'INNER JOIN %s ON %s.%s = %s.id '%(box_list, 
+                                                        table, id,
+                                                        box_list)
+            
+        if len(self.rulestack) > 0:
+            query += 'WHERE '
+            conditions = []
+            for attrib in self.rulestack:
+                keys = [repr(s) for s in attrib['k'].split('|')]
 
-        for attrib in self.rulestack:
-            keys = [repr(s) for s in attrib['k'].split('|')]
+                if attrib['v'] == '~':
+                    conditions.append('k NOT IN (%s)'%', '.join(keys))
+                else:
+                    if '*' not in attrib['k']:
+                        conditions.append('k IN (%s)'%', '.join(keys))
 
-            if attrib['v'] == '~':
-                conditions.append('k NOT IN (%s)'%', '.join(keys))
-            else:
-                if '*' not in attrib['k']:
-                    conditions.append('k IN (%s)'%', '.join(keys))
-                
-                if attrib['v'] != '*':
-                    values = [repr(s) for s in attrib['v'].split('|')]
-                    conditions.append('v IN (%s)'%', '.join(values))
+                    if attrib['v'] != '*':
+                        values = [repr(s) for s in attrib['v'].split('|')]
+                        conditions.append('v IN (%s)'%', '.join(values))
 
-        query += ' AND '.join(conditions)
+            query += ' AND '.join(conditions)
 
         return query
 
-
 def build_sql_queries(filename, bbox=None):
-    target = SQLBuilder()
+    target = SQLBuilder(box_node_list='box_node_list',
+                        box_way_list='box_way_list')
+    init_queries = []
+
+    ## If bounding box is given, create a temporary table and
+    ## store all matching nodes there
+    if bbox != None:
+        ## Create tables
+        init_queries.append("CREATE TEMPORARY TABLE box_node_list "
+                            "(id bigint PRIMARY KEY) ON COMMIT DROP")
+        init_queries.append("CREATE TEMPORARY TABLE box_way_list "
+                            "(id bigint PRIMARY KEY) ON COMMIT DROP")
+        
+        ## Fill them
+        bbox_sql = "GeomFromEWKT('POLYGON((%f %f,%f %f,%f %f,%f %f,%f %f))')"%\
+            (bbox[0],bbox[1],bbox[2],bbox[1],bbox[2],bbox[3],bbox[0],bbox[3],
+             bbox[0],bbox[1])
+
+        srid = "find_srid('','nodes','geom')"
+
+        init_queries.append("INSERT INTO box_node_list SELECT id FROM nodes "
+                            "WHERE (geom && SetSRID(%s,%s) )"%(bbox_sql,srid))
+
+        init_queries.append("INSERT INTO box_way_list "
+                            "SELECT wn.way_id FROM way_nodes wn "
+                            "INNER JOIN box_node_list n ON wn.node_id = n.id "
+                            "GROUP BY wn.way_id")
 
     parser = XMLTreeBuilder(target=target)
 
     parser.feed(open(filename).read())
 
-    return target.result
+    return init_queries, target.result
 
 class MapBuilderSQL(osm.MapBuilder):
     def __init__(self, rules, mapobj, bbox,
@@ -93,7 +127,13 @@ class MapBuilderSQL(osm.MapBuilder):
     def load(self):
         self.connect()
 
-        for rule in build_sql_queries(self.rules.filename, self.bbox):
+        init_queries, rules = build_sql_queries(self.rules.filename, self.bbox)
+
+        for query in init_queries:
+            cursor = self.dbh.cursor()
+            cursor.execute(query)
+
+        for rule in rules:
             statement, element, query = rule
             
             cursor = self.dbh.cursor()
@@ -111,7 +151,8 @@ class MapBuilderSQL(osm.MapBuilder):
                     tags = dict(cursor_tags.fetchall())
 
                     ## Get node coordinates
-                    query = "SELECT X(nodes.geom), Y(nodes.geom) FROM nodes, way_nodes " \
+                    query = "SELECT X(nodes.geom), Y(nodes.geom) " \
+                            "FROM nodes, way_nodes " \
                             "WHERE nodes.id = way_nodes.node_id AND "\
                             "   way_nodes.way_id = %s " \
                             "ORDER BY way_nodes.sequence_id"%way[0]
@@ -164,4 +205,4 @@ if __name__ == "__main__":
     import doctest
     doctest.testmod()
 
-    print build_sql_queries('data/osmmagrules.xml')
+    print build_sql_queries('data/osmmagrules.xml', bbox = [10,20,30,40])
